87e682611993d9a24c7efd8f64a495883aeafd0f
George Chang <changg@gmail.com>
2018-10-18 22:55:28 -0400

    Initial commit

---
+++ Cake.Unicorn/Unicorn.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+using MicroCHAP;
+
+namespace Cake.Unicorn
+{
+    public static class Unicorn
+    {
+        [CakeMethodAlias]
+        public static async void SyncUnicorn(this ICakeContext context, UnicornSettings settings)
+        {
+            var syncUrl = $"{settings.ControlPanelUrl}?verb={settings.Verb}&configuration={settings.GetParsedConfigurations()}&skipTransparentConfigs={settings.SkipTransparentConfigs}";
+
+            var challenge = await GetChallengeAsync(settings.ControlPanelUrl, context.Log);
+            var signature = CreateSignature(challenge, settings.SharedSecret, syncUrl, context.Log);
+            var response = await ExecuteUnicornAsync(syncUrl, signature, challenge, context.Log);
+        }
+
+        public static async Task<string> GetChallengeAsync(string controlPanelUrl, ICakeLog log)
+        {
+            using (var client = new HttpClient())
+            {
+                var challenge = await client.GetStringAsync($"{controlPanelUrl}?verb=Challenge");
+                log.Debug($"Received challenge from remote server: {challenge}");
+                return challenge;
+            }
+        }
+
+        public static string CreateSignature(string challenge, string sharedSecret, string syncUrl, ICakeLog log)
+        {
+            var service = new SignatureService(sharedSecret);
+            var signature = service.CreateSignature(challenge, syncUrl, null);
+            log.Debug($"MAC: '{signature.SignatureSource}'");
+            log.Debug($"HMAC: '{signature.SignatureHash}'");
+            log.Debug("If you get authorization failures compare the values above to the Sitecore logs.");
+            return signature.SignatureHash;
+        }
+
+        public static async Task<string> ExecuteUnicornAsync(string syncUrl, string signature, string challenge, ICakeLog log)
+        {
+            log.Write(Verbosity.Normal, LogLevel.Information, "Executing Unicorn...");
+            var responseStringBuilder = new StringBuilder();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-MC-MAC", signature);
+                client.DefaultRequestHeaders.Add("X-MC-NONCE", challenge);
+                client.Timeout = new TimeSpan(10800000);
+
+                var response = await client.GetAsync(syncUrl);
+                log.Verbose(response);
+
+                return response.Content.ToString();
+            }
+        }
+    }
+}
---
+++ Cake.Unicorn/UnicornSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cake.Core.Tooling;
+
+namespace Cake.Unicorn
+{
+    public class UnicornSettings : ToolSettings
+    {
+        public string ControlPanelUrl { get; set; }
+        public string SharedSecret { get; set; }
+        public IEnumerable<string> Configurations { get; set; }
+        public bool SkipTransparentConfigs { get; set; } = false;
+        public string Verb { get; set; } = "Sync";
+        public string GetParsedConfigurations() => Configurations != null ? string.Join("^", Configurations) : string.Empty;
+    }
+}
